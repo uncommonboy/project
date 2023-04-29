ac0919b234c6ed2f19f20ca5ef960c6dc72702ee
uncommonboy <abdugafforovasilbek206@gmail.com>
2023-04-29 10:06:27 +0500

    logins

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const btn = document.querySelector(".header__menubtn");
 const menu = document.querySelector(".header__mbtn");
 const icon_menu = document.querySelector("#icon");
 const icon_cancel = document.querySelector("#cancel");
-const items = document.querySelectorAll(".js-item");
 
 icon_menu.addEventListener("click", func);
 function func() {
@@ -20,17 +18,9 @@ function func2() {
   icon_menu.style.display = "block";
 }
 
-for (let i = 0; i < items.length; i++) {
-  items[i].addEventListener("click", () => {
-    console.log("ishledi");
-    alert("Please first Log-In to see your blongings!");
-  });
-}
 
-const button = document.querySelector(".subscribe__button");
-button.addEventListener("click", () => {
-  const inp = document.querySelector(".subscribe__input");
-  inp.value = null;
-  alert("response send successfully!");
-  console.log(inp.value);
-});
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
