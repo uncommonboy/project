1a23c3ac41b882ba899b71964083b7106d9fb808
uncommonboy <abdugafforovasilbek206@gmail.com>
2023-04-30 16:26:25 +0500

    background changed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const entry = document.querySelector(".wrap1");
+const sign_in = document.querySelector(".sign__in");
 
 const numb = "93-531-08-98";
-const password = "udevs1234";
+const password = "asilbek1234";
 
 entry.addEventListener("click", (e) => {
   if (e.target.closest(".responses__button1")) {
@@ -19,11 +20,11 @@ entry.addEventListener("click", (e) => {
   }
 });
 
-const sign_in = document.querySelector(".sign__in");
 sign_in.addEventListener("click", (e) => {
   const numb_inp = document.querySelector(".number__inp");
   const password_inp = document.querySelector(".password__inp");
   const togglePassword = document.querySelector(".main__passshow");
+  const cancel = document.querySelector(".main__cancel");
   if (e.target.closest(".main__button")) {
     if (numb_inp.value == "") {
       console.log("salom");
@@ -31,19 +32,30 @@ sign_in.addEventListener("click", (e) => {
     } else if (password_inp.value == "") {
       alert("Enter Password!!!");
     }
-    if(numb_inp.value==numb && password_inp.value==password){
-      const wraper=document.querySelector('.wraper')
-      wraper.style.display='flex'
-      sign_in.style.display='none'
+    if (numb_inp.value == numb && password_inp.value == password) {
+      const wraper = document.querySelector(".wraper");
+      wraper.style.display = "flex";
+      sign_in.style.display = "none";
+    } else if (numb_inp.value != numb) {
+      alert("Number Is Incorrect ");
+    } else if (password_inp.value != password) {
+      alert("Password Is Incorrect ");
     }
   }
-  if(e.target.closest('.main__passshow')){
-    togglePassword.addEventListener("click", ()=> {
+  if (e.target.closest(".main__passshow")) {
+    togglePassword.addEventListener("click", () => {
       if (password_inp.type === "password") {
         password_inp.type = "text";
       } else {
         password_inp.type = "password";
       }
+    });
+  }
+  if (e.target.closest(".main__cancel")) {
+    cancel.addEventListener("click", () => {
+      console.log("salom");
+      sign_in.style.display = "none";
+      entry.style.display = "block ";
     });
   }
 });
